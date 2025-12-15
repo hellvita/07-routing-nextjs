31b96ce65120aa28aa34b661ae67b02ff9042fac
hellvita <seredaolga03@gmail.com>
2025-12-15 15:51:42 +0200

    add the back button to the NotePreview component

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -42,14 +42,19 @@ export default function NotePreviewClient() {
         </p>
       )}
       {note && (
-        <div className={css.container}>
-          <div className={css.item}>
-            <div className={css.header}>
-              <h2>{note.title}</h2>
+        <div>
+          <div className={css.container}>
+            <div className={css.item}>
+              <div className={css.header}>
+                <h2>{note.title}</h2>
+              </div>
+              <p className={css.content}>{note.content}</p>
+              <p className={css.date}>{formattedDate}</p>
             </div>
-            <p className={css.content}>{note.content}</p>
-            <p className={css.date}>{formattedDate}</p>
           </div>
+          <button onClick={handleClose} className={css.btn}>
+            {"<< "} back
+          </button>
         </div>
       )}
     </Modal>
